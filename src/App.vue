f43ee14de3c543c132c4ad7c6832539815db10d4
yjh <614884674@qq.com>
2023-08-17 23:39:13 +0800

    头部导航栏添加

---
+++ src/App.vue
@@ -0,0 +1,99 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <div class="header">
+    <img alt="logo" class="logo" src="@/assets/logo.svg"/>
+  </div>
+  <!-- <header> -->
+    <!-- <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125px" height="125px" /> -->
+    
+    <!-- <div class="wrapper">
+      <HelloWorld msg="You did it!" /> -->
+
+      <!-- <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+      </nav> -->
+    <!-- </div> -->
+  <!-- </header> -->
+
+  <!-- <RouterView /> -->
+</template>
+
+<style scoped>
+.header{
+  display:flex;  
+	justify-content:center; 
+	align-items:center;  
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  background-color: #242f42;
+  height: 70px;
+}
+
+
+.logo{
+  
+    width: 70px;
+    height: 70px;
+    /* float: left; */
+    margin: 30px;
+}
+/*
+nav {
+  width: 100%;
+  font-size: 12px;
+  text-align: center;
+  margin-top: 2rem;
+}
+
+nav a.router-link-exact-active {
+  color: var(--color-text);
+}
+
+nav a.router-link-exact-active:hover {
+  background-color: transparent;
+}
+
+nav a {
+  display: inline-block;
+  padding: 0 1rem;
+  border-left: 1px solid var(--color-border);
+}
+
+nav a:first-of-type {
+  border: 0;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+
+  nav {
+    text-align: left;
+    margin-left: -1rem;
+    font-size: 1rem;
+
+    padding: 1rem 0;
+    margin-top: 1rem;
+  }
+} */
+</style>
